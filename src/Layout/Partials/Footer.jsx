88b40d13f7764572dec30d7ac060dc9064f79960
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-06-12 01:54:39 -0700

    Fixed footer

--- src/Layout/Partials/Footer.jsx
+++ src/Layout/Partials/Footer.jsx
@@ -1,13 +1,33 @@
-import React from 'react';
+import React from "react";
 
 export default function Footer() {
 	return (
 		<div>
 			<footer className="boxshadow">
 				<ul className="updated">
-					<li><cite>&copy; 2020-{new Date().getFullYear()} {process.env.REACT_APP_AUTHOR}</cite></li>
-					<li id="print"><i className="material-icons" onClick={() => {window.print(); return false}} title="Opens the print dialog box.">print</i></li>
-					<li><a href="#top" className="scroll" data-speed="500">Top</a></li>
+					<li>
+						<cite>
+							&copy; 2020-{new Date().getFullYear()}{" "}
+							{process.env.REACT_APP_AUTHOR}
+						</cite>
+					</li>
+					<li id="print">
+						<i
+							className="material-icons"
+							onClick={() => {
+								window.print();
+								return false;
+							}}
+							title="Opens the print dialog box."
+						>
+							print
+						</i>
+					</li>
+					<li>
+						<a href="#top" className="scroll" data-speed="500">
+							Top
+						</a>
+					</li>
 				</ul>
 			</footer>
 		</div>
