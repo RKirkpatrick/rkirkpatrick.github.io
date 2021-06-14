0639e2d92e2dea1d1d80167caa701de31d4e5ef5
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-06-13 19:38:15 -0700

    Added caption to masnory

--- src/Layout/Partials/Masonry.jsx
+++ src/Layout/Partials/Masonry.jsx
@@ -1,13 +1,15 @@
 const Image = ({ path, lowRes, highRes, description, author, year }) => {
 	return (
-		<a href={path + highRes} target="_blank" rel="noopener noreferrer">
-			<img
-				src={path + lowRes}
-				alt={description}
-				title={"\u00A9 " + author + " " + year}
-				className="caption"
-			/>
-		</a>
+		<figure>
+			<a href={path + highRes} target="_blank" rel="noopener noreferrer">
+				<img
+					src={path + lowRes}
+					alt={description}
+					title={"\u00A9 " + author + " " + year}
+				/>
+			</a>
+			<figcaption>{description}</figcaption>
+		</figure>
 	);
 };
 
