2b2ac696dbb89378614bf5766c06c270ee9b08dd
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-06-13 19:38:52 -0700

    Added video component

---
+++ src/Layout/Partials/Video.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+
+const Video = ({ src }) => {
+	return (
+		<iframe
+			title="Embedded video"
+			src={src}
+			className="aligncenter"
+			allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+			allowFullscreen
+		/>
+	);
+};
+
+Video.propTypes = {
+	src: PropTypes.string.isRequired,
+};
+
+export default Video;
