cde1483dd3200edbb4430693c1ac2e5b416bcdae
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-06-16 00:51:55 -0700

    Highlight after redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 import hljs from "highlight.js/lib/common";
 import "highlight.js/styles/default.css";
 import React, { useEffect } from "react";
-import { Redirect, Route, Switch, useHistory } from "react-router-dom";
+import { Route, Switch, useHistory } from "react-router-dom";
 import "./App.css";
 import Layout from "./Layout";
 
 function App() {
 	let history = useHistory();
 	useEffect(() => {
-		document.querySelectorAll("pre code").forEach((element) => {
-			hljs.highlightElement(element);
-		});
 		let path = localStorage.getItem("path");
 		console.log("Path:", path);
 		if (path) {
 			localStorage.removeItem("path");
 			history.replace(path);
 		}
+
+		document.querySelectorAll("pre code").forEach((element) => {
+			hljs.highlightElement(element);
+		});
 	}, []);
 
 	return (
