8ed0d2095df444babb968d808e593481030febc0
Ryan Kirkpatrick <Rk168810@gmail.com>
2021-06-19 03:23:27 -0700

    Added python script to convert markdown to a react string

---
+++ convertMarkdown.py
@@ -0,0 +1,10 @@
+input = open("./test.txt", "r")
+output = open("./test2.txt", "w")
+secondLastLine, lastLine = input.readline(), input.readline()
+for line in input:
+	output.write("\"" + secondLastLine.replace("\"", "\\\"").rstrip("\n") + "\\n\" +\n")
+	secondLastLine = lastLine
+	lastLine = line
+output.write("\"" + lastLine.replace("\"", "'").rstrip("\n") + "\"")
+input.close()
+output.close()
