d80f702421601604c7ba7b10a4fc2ff397141ed5
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-03-05 19:08:12 -0800

    Added css variables to toggle function

--- src/Layout/Common/Darkmode.jsx
+++ src/Layout/Common/Darkmode.jsx
@@ -14,58 +14,55 @@ export default function DarkmodeToggle() {
 		console.log("Darkmode changed");
 		if (isDarkmode) {
 			localStorage["darkmode"] = "true";
-			// document.documentElement.style.setProperty("--backgroundcolor", "black");
-			// document.documentElement.style.setProperty("--textcolor", "white");
-			// document.documentElement.style.setProperty("--textboxfocuscolor", "");
-			// document.documentElement.style.setProperty("--navbackground", "");
-			// document.documentElement.style.setProperty("--navbackgroundhover", "");
-			// document.documentElement.style.setProperty("--navtext", "white");
-			// document.documentElement.style.setProperty("--navtexthover", "");
-			// document.documentElement.style.setProperty("--navtextactive", "");
-			// document.documentElement.style.setProperty("--navchildbackground", "");
-			// document.documentElement.style.setProperty(
-			// 	"--navchildbackgroundhover",
-			// 	""
-			// );
-			// document.documentElement.style.setProperty("--navchildtext", "");
-			// document.documentElement.style.setProperty("--navchildtexthover", "");
-			// document.documentElement.style.setProperty("--navchildtextactive", "");
+			document.documentElement.style.setProperty("--backgroundcolor", "black");
+			document.documentElement.style.setProperty("--textcolor", "white");
+			document.documentElement.style.setProperty(
+				"--boxshadowcolor",
+				"rgba(255,255,255,1)"
+			);
+			document.documentElement.style.setProperty("--textboxfocuscolor", "");
+			document.documentElement.style.setProperty("--navbackground", "black");
+			document.documentElement.style.setProperty("--navbackgroundhover", "");
+			document.documentElement.style.setProperty("--navtext", "white");
+			document.documentElement.style.setProperty("--navtexthover", "");
+			document.documentElement.style.setProperty("--navtextactive", "");
+			document.documentElement.style.setProperty("--navchildbackground", "");
+			document.documentElement.style.setProperty(
+				"--navchildbackgroundhover",
+				""
+			);
+			document.documentElement.style.setProperty("--navchildtext", "");
+			document.documentElement.style.setProperty("--navchildtexthover", "");
+			document.documentElement.style.setProperty("--navchildtextactive", "");
 		} else {
 			localStorage["darkmode"] = "false";
-			// document.documentElement.style.setProperty("--backgroundcolor", "");
-			// document.documentElement.style.setProperty("--textcolor", "");
-			// document.documentElement.style.setProperty("--colspancolor", "");
-			// document.documentElement.style.setProperty("--textboxfocuscolor", "");
-			// document.documentElement.style.setProperty("--navbackground", "");
-			// document.documentElement.style.setProperty("--navbackgroundhover", "");
-			// document.documentElement.style.setProperty("--navtext", "");
-			// document.documentElement.style.setProperty("--navtexthover", "");
-			// document.documentElement.style.setProperty("--navtextactive", "");
-			// document.documentElement.style.setProperty("--navchildbackground", "");
-			// document.documentElement.style.setProperty(
-			// 	"--navchildbackgroundhover",
-			// 	""
-			// );
-			// document.documentElement.style.setProperty("--navchildtext", "");
-			// document.documentElement.style.setProperty("--navchildtexthover", "");
-			// document.documentElement.style.setProperty("--navchildtextactive", "");
+			document.documentElement.style.setProperty("--backgroundcolor", "");
+			document.documentElement.style.setProperty("--textcolor", "");
+			document.documentElement.style.setProperty("--boxshadowcolor", "");
+			document.documentElement.style.setProperty("--textboxfocuscolor", "");
+			document.documentElement.style.setProperty("--navbackground", "");
+			document.documentElement.style.setProperty("--navbackgroundhover", "");
+			document.documentElement.style.setProperty("--navtext", "");
+			document.documentElement.style.setProperty("--navtexthover", "");
+			document.documentElement.style.setProperty("--navtextactive", "");
+			document.documentElement.style.setProperty("--navchildbackground", "");
+			document.documentElement.style.setProperty(
+				"--navchildbackgroundhover",
+				""
+			);
+			document.documentElement.style.setProperty("--navchildtext", "");
+			document.documentElement.style.setProperty("--navchildtexthover", "");
+			document.documentElement.style.setProperty("--navchildtextactive", "");
 		}
-		document.documentElement.style.setProperty("--backgroundcolor", "black");
-		document.documentElement.style.setProperty("--textcolor", "white");
-		console.log("Local:", localStorage["darkmode"]);
 	}, [isDarkmode]);
 
-	function toggleDarkMode() {
-		setDarkmode(!isDarkmode);
-		document.documentElement.style.setProperty("--backgroundcolor", "black");
-		document.documentElement.style.setProperty("--textcolor", "white");
-	}
+	function toggleDarkMode() {}
 
 	return (
 		<input
 			className="l"
 			type="checkbox"
-			onClick={toggleDarkMode}
+			onClick={() => setDarkmode(!isDarkmode)}
 			checked={!isDarkmode}
 			readOnly
 		/>
