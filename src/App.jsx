67fe16dd16bbd290d6eb57d24a62f7b7b27c2299
Ryan Kirkpatrick <Rk168810@gmail.com>
2022-07-25 19:15:58 -0700

    Moved neumorphic design to it's own css file

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useEffect } from "react";
 import { Route, Switch, useHistory } from "react-router-dom";
 import "./App.css";
 import "./Layout/Common/Menu.css";
+import "./Layout/Common/Neumorphic.css";
 import Layout from "./Layout";
 
 function App() {
