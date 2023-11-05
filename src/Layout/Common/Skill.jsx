fca89b143bf3d91122ee47defe30cdca9ba8ce67
Ryan Kirkpatrick <Rk168810@gmail.com>
2023-11-05 13:09:15 -0800

    Deleted old skill code

--- src/Layout/Common/Skill.jsx
+++ src/Layout/Common/Skill.jsx
@@ -1,23 +1,5 @@
 import React from "react";
 
-// export default function Skill({ name, years, colSpan }) {
-// 	let yearStr = years > 1 ? "years" : "year";
-// 	let columns = [];
-
-// 	for (let i = 0; i < 4 - colSpan; i++) {
-// 		columns.push(<td key={i}></td>);
-// 	}
-
-// 	return (
-// 		<tr>
-// 			<th>{name}</th>
-// 			<td colSpan={colSpan}>
-// 				{years} {yearStr} of experience
-// 			</td>
-// 			{columns}
-// 		</tr>
-// 	);
-// }
 export default function Skill({
 	name,
 	years = 0,
